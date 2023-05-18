174b4c09f07ee730d12261b40c898b2104df61d5
iambasanta <sangpangbasanta@gmail.com>
2023-05-18 17:23:11 +0545

    Add basic scaffolding

---
+++ src/public/index.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Welcome to the Home page!</h1> 
+        <a href="/" >Home</a>
+        <a href="/register.php" >Register</a>
+        <a href="/login.php" >Login</a>
+        <a href="/memes.php" >Memes</a>
+    </body>
+</html>
+
---
+++ src/public/memes.php
@@ -0,0 +1,3 @@
+<?php
+
+    require("memes.view.php");
